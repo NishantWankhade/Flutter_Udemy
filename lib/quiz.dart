fd9bcedf805cec7f39336d6c69df60eb4c8b16f9
NGW334 <ngw334@gmail.com>
2022-12-04 10:30:49 +0530

    Adding Project

---
+++ lib/quiz.dart
@@ -0,0 +1,34 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import './answer.dart';
+import './question.dart';
+
+class Quiz extends StatelessWidget {
+  final Function answerQuestion;
+  final int questionIndex;
+  final List<Map<String, Object>> questions;
+
+  Quiz({
+    @required this.questions,
+    @required this.answerQuestion,
+    @required this.questionIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: <Widget>[
+        Question(
+          questions[questionIndex]['questionText'],
+        ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(
+              () => answerQuestion(answer['score']), answer['text'] as String);
+        }).toList()
+      ],
+    ));
+  }
+}
